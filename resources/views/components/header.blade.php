d973571ea27f1f189d4936a2707883a342626385
JunLim <158136143+LimJun99@users.noreply.github.com>
2025-04-21 11:46:09 +0800

    FIx header.blade.php

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -2,8 +2,10 @@
 
      {{-- Header bar --}}
      <header class="header">
+          
+
           <button class="sidebar-button" id="sidebar-button">
-               <span class="material-icons-outlined">&#xe5d2;</span>
+               <!-- <span class="material-icons-outlined" style="cursor: pointer;">&#xe5d2;</span> -->
           </button>
 
           @isset($trash)
@@ -15,7 +17,7 @@
           @else
                <form action="{{ route('entries.search') }}" method="post" class="input-search">
                     @csrf
-                    <input type="text" name="search" id="entry-name" placeholder=" &#xf002 Search your entries" style="font-family:Arial, FontAwesome" value="@isset($search){{ $search }}@endisset" >
+                    <input type="text" name="search" id="entry-name" placeholder=" &#xf002 Search your entries" style="font-family:Arial, FontAwesome" value="@isset($search){{ $search }}@endisset" required>
                     <button type="submit">
                          <span class="material-icons-outlined">&#xf1df;</span>
                     </button>
@@ -24,7 +26,7 @@
 
           <div class="right-component">
                <div class="profile-image">
-                    <img src="{{ $user->image ? asset($user->image->path) : asset('/img/image-defaut.png') }}" alt="Profile Image" class="dropdown-user-img">
+                    <img src="@if($user->image) {{$user->image->path}} @else {{asset('/img/image-defaut.png')}} @endif" alt="Undisplayable image" class="dropdown-user-img">
                </div>
           </div>
      </header>
@@ -32,11 +34,11 @@
      {{-- Dropdown user --}}
      <div class="dropdown">
           <div class="user-data">
-               <a href="{{ route('user.profile')}}" class="user-image">
-                    <img src="{{ $user->image ? asset($user->image->path) : asset('/img/image-defaut.png') }}" alt="Profile Image">
+               <a href="{{ route('user.profile') }}" class="user-image">
+                    <img src="@if($user->image) {{$user->image->path}} @else {{asset('/img/image-defaut.png')}} @endif" alt="Undisplayable image">
                     <button class="material-icons-outlined icon-camera">&#xe3c9;</button>
                </a>
-               
+
                <h2 class="username">{{$user->name}}</h2>
                <span class="email">{{$user->email}}</span>
           </div>
@@ -49,3 +51,61 @@
           <span class="material-icons-outlined close-bttn">&#xe5cd;</span>
      </div>
 </div>
+
+<header class="header-note">
+     <div class="container">
+
+          @isset($readOnly)
+               <a href="{{ route('entries.trash') }}" class="material-icons-outlined icons">&#xe5c4;</a>
+          @else
+               <a href="{{ route('entries.index') }}" class="material-icons-outlined icons">&#xe5c4;</a>
+          @endisset
+
+          @if($method == 'put')
+               @isset($readOnly)
+                    <form action="{{ route('entries.restore', $entry) }}" method="post" id="entry-form">
+                         @csrf
+                         @method('put')
+                         @include('entries.components.headerForm')
+                         <select name="emotion" class="emotion-dropdown">
+                              <option value="happy">Happy</option>
+                              <option value="sad">Sad</option>
+                              <option value="neutral">Neutral</option>
+                         </select>
+                    </form>
+               @else
+                    <form action="{{ route('entries.update', $entry) }}" method="post" id="entry-form">
+                         @csrf
+                         @method('put')
+                         @include('entries.components.headerForm')
+                         <select name="emotion" class="emotion-dropdown">
+                              <option value="happy">Happy</option>
+                              <option value="sad">Sad</option>
+                              <option value="neutral">Neutral</option>
+                         </select>
+                    </form>
+               @endisset
+          @else
+               <form action="{{ route('entries.store') }}" method="post" id="entry-form">
+                    @csrf
+                    @method('post')
+                    @include('entries.components.headerForm')
+               </form>
+          @endif
+
+          @isset($edit)
+               <div class="dropdown-options dropdown-editor">
+                    <a href="" class="menu-item">Add label</a>
+
+                    @if($method == 'put')
+                         <form action="{{ route('entries.sendTrash', $entry) }}" method="post">
+                              @csrf
+                              @method('delete')
+                              <button type="submit" class="menu-item">Delete Entry</button>
+                         </form>
+                         <a href="{{ route('entries.make_copy', $entry) }}" class="menu-item">Make a copy</a>
+                    @endif
+               </div>
+          @endisset
+     </div>
+</header>
